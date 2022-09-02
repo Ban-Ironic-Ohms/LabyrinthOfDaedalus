25b8b2a62d94b95df1f06d74986218b68da4c1f3
owenstamo <owenstamo@me.com>
2022-09-01 17:47:24 -0700

    making room class a child of poi, merging descriptions classes into one

--- Rooms/CLASSES ARE BETTER.py
+++ Rooms/CLASSES ARE BETTER.py
@@ -2,32 +2,39 @@ from __future__ import annotations
 import json
 import random
 
-class Descriptions:
-    def __init__(self, main_description=""):
-        self.main = main_description
-
 def load_file(file_name: str) -> dict:
     with open(file_name, "w") as f:
         return json.load(f)
 
-class Room:
-    def __init__(self, room_data: dict):
-        self.data = room_data
+class Descriptions:
+    DEFAULT = "unimplemented description"
+
+    def __init__(self, main=DEFAULT, attack=DEFAULT, door=DEFAULT):
+        self.main = main
+        self.attack = attack
+        self.door = door
 
 class Poi:
+    @staticmethod
+    def get_poi_type(poi_data):
+        if "room" in poi_data["class"]:
+            return Room(poi_data)
+        if "enemy" in poi_data["class"]:
+            return Enemy(poi_data)
+        else:
+            return Entity(poi_data)
+
     def __init__(self, poi_data: dict):
         self.data = poi_data
 
-class Entity:
-    @staticmethod
-    def get_entity(entity_data):
-        if "enemy" in entity_data["class"]:
-            return Enemy(entity_data)
-        else:
-            return Entity(entity_data)
+        self.child_pois = [self.get_poi_type(child_poi_data) for child_poi_data]
 
+class Room(Poi):
+    ...
+
+class Entity(Poi):
     def __init__(self, entity_data):
-        self.data = entity_data
+        super().__init__(entity_data)
 
     @property
     def name(self):
@@ -39,13 +46,9 @@ class Entity:
 
     @property
     def descriptions(self):
-        return Descriptions(self.data["descriptions"]["main_description"])
+        return Descriptions(main=self.data["descriptions"]["main_description"])
 
 class Enemy(Entity):
-    class EnemyDescriptions(Descriptions):
-        def __init__(self, main_description="", attack_description=""):
-            super().__init__(main_description)
-            self.attack = attack_description
 
     @property
     def hp(self):
@@ -61,9 +64,9 @@ class Enemy(Entity):
 
     @property
     def descriptions(self):
-        return self.EnemyDescriptions(
-            self.data["descriptions"]["main_description"],
-            self.data["descriptions"]["attack_description"]
+        return Descriptions(
+            main=self.data["descriptions"]["main_description"],
+            attack=self.data["descriptions"]["attack_description"]
         )
 
     def combat(self, player: Player, input_handler):
@@ -100,11 +103,6 @@ class Enemy(Entity):
 
 
 class Player:
-    class PlayerDescriptions(Descriptions):
-        def __init__(self, main_description, attack_description):
-            super().__init__(main_description)
-            self.attack = attack_description
-
     def __init__(self, player_data: dict):
         self.data = player_data
 
@@ -134,9 +132,9 @@ class Player:
 
     @property
     def descriptions(self):
-        return self.PlayerDescriptions(
-            self.data["descriptions"]["main_description"],
-            self.data["descriptions"]["attack_description"]
+        return Descriptions(
+            main=self.data["descriptions"]["main_description"],
+            attack=self.data["descriptions"]["attack_description"]
         )
 
     def add_to_inventory(self, item):
