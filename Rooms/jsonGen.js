c75c0e0d918538ee1f58653a197a120248d3ef09
Stinnes <807064@suhsd-staff.seq.org>
2022-09-09 11:11:33 -0700

    hi

---
+++ Rooms/jsonGen.js
@@ -0,0 +1,6 @@
+
+document.querySelector("#Input").addEventListener("change", Set, true)
+
+function Set() {
+    document.querySelector("#Show").innerHTML = document.querySelector('#Input').value;
+}
