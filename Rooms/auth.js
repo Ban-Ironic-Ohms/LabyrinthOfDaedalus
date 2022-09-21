68708a03e31b61e7feec3b9f596e3f943097ef2d
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2022-09-21 09:13:02 -0700

    basic auth working - users can be added to database

--- Rooms/auth.js
+++ Rooms/auth.js
@@ -1,9 +1,20 @@
-// import firebase from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
-// import firebaseui from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
-import firebase from "../node_modules/firebase/firebase-app.js";
-import firebaseui from "../node_modules/firebaseui/dist/firebaseui.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
 
-console.log("imported")
+const firebaseConfig = {
+  apiKey: "AIzaSyAy70cBoQiHhkKCu2DQBiA31HOlEiSMNpY",
+  authDomain: "labyrinthofdaedalus-79a5f.firebaseapp.com",
+  databaseURL: "https://labyrinthofdaedalus-79a5f-default-rtdb.firebaseio.com",
+  projectId: "labyrinthofdaedalus-79a5f",
+  storageBucket: "labyrinthofdaedalus-79a5f.appspot.com",
+  messagingSenderId: "231533789818",
+  appId: "1:231533789818:web:7121ae54c94e1dd30ef1c3",
+  measurementId: "G-Q1QNG1SH1Q"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+console.log("auth.js loaded");
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
