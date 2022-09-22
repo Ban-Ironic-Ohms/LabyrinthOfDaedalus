e67511cd7dd3c4702ebf51ff87c1095014a0857c
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2022-09-22 14:01:17 -0700

    auth still not working 100%

--- Rooms/auth.js
+++ Rooms/auth.js
@@ -1,22 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "../node_modules/firebase/firebase-auth.js";
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAy70cBoQiHhkKCu2DQBiA31HOlEiSMNpY",
-  authDomain: "labyrinthofdaedalus-79a5f.firebaseapp.com",
-  databaseURL: "https://labyrinthofdaedalus-79a5f-default-rtdb.firebaseio.com",
-  projectId: "labyrinthofdaedalus-79a5f",
-  storageBucket: "labyrinthofdaedalus-79a5f.appspot.com",
-  messagingSenderId: "231533789818",
-  appId: "1:231533789818:web:7121ae54c94e1dd30ef1c3",
-  measurementId: "G-Q1QNG1SH1Q"
-};
-
-
-firebase.initializeApp(firebaseConfig);
-
-console.log("auth.js loaded");
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
@@ -25,33 +9,33 @@ ui.start('#firebaseui-auth-container', {
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Other config options...
   });
   
-
-var uiConfig = {
-callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-    // User successfully signed in.
+  
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
     // Return type determines whether we continue the redirect automatically
     // or whether we leave that to developer to handle.
-    return true;
-    },
-    uiShown: function() {
+    return false;
+  },
+  uiShown: function() {
     // The widget is rendered.
     // Hide the loader.
     document.getElementById('loader').style.display = 'none';
-    }
+  }
 },
 // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
 signInFlow: 'popup',
-signInSuccessUrl: 'jsonGenWeb.html',
+signInSuccessUrl: 'auth.html',
 signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  firebase.auth.EmailAuthProvider.PROVIDER_ID,
 ],
 
 // Terms of service url.
@@ -63,13 +47,61 @@ privacyPolicyUrl: 'google.com'
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
 
+function printUserInfo(user=auth.user) {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    const email = user.email;
+    const emailVerified = user.emailVerified;
+    const displayName = user.displayName;
+    // const photoURL = user.photoURL;
+    // const phoneNumber = user.phoneNumber;
+    // const isAnonymous = user.isAnonymous;
+    // const tenantId = user.tenantId;
+    // const providerData = user.providerData;
+    const token = user.getIdToken();
+    console.log("uid" + uid);
+    console.log("email" + email);
+    console.log("emailVerified" + emailVerified);
+    console.log("displayName" + displayName);
+    console.log("token" + token);
+  }
+}
+
+console.log("auth.js loaded");
 const auth = getAuth();
+console.log(auth)
+
+function getUserInfo() {
+  const user = auth.currentUser;
+
+  if (user) {
+    console.log("this runs")
+  }
+}
+getUserInfo()
+
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
-    console.log("uid")
+    const email = user.email;
+    const emailVerified = user.emailVerified;
+    const displayName = user.displayName;
+    // const photoURL = user.photoURL;
+    // const phoneNumber = user.phoneNumber;
+    // const isAnonymous = user.isAnonymous;
+    // const tenantId = user.tenantId;
+    // const providerData = user.providerData;
+    const token = user.getIdToken();
+    console.log("uid" + uid);
+    console.log("email" + email);
+    console.log("emailVerified" + emailVerified);
+    console.log("displayName" + displayName);
+    console.log("token" + token);
+
   } else {
     console.log("DEATH TO USERS")
   }
